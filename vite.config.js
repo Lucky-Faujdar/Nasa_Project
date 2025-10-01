1d496dfce6c619fbc2d567391301f008cfe9537a
Lucky <lucky114005@gmail.com>
2025-10-01 22:45:08 +0530

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss(),],
-})
+  server: {
+    allowedHosts: [
+      // Add the Render URL of your deployed frontend
+      'nasa-project-6byr.onrender.com' 
+    ],
+  },
+});
