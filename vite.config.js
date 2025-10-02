d61bb558746952d7ef4635a6ec2bf8018af71a09
Lucky <lucky114005@gmail.com>
2025-10-02 15:28:26 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react() , tailwindcss(),],
   server: {
-    proxy: {
-      "/api": "http://localhost:5000", // local dev backend
+     proxy: {
+      "/api": "http://localhost:5000",
     },
     allowedHosts: [
-      'nasa-project-6byr.onrender.com', // your deployed frontend domain
+      // Add the Render URL of your deployed frontend
+      'nasa-project-6byr.onrender.com',
       'localhost'
     ],
   },
